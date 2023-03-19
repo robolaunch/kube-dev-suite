ec429e6d4ea338cbd04b8322b45cc38f825b5e9b
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-03-20 02:03:37 +0300

    refactor(image): update image selection

--- internal/node/image.go
+++ internal/node/image.go
@@ -39,7 +39,7 @@ func GetImage(node corev1.Node, robot robotv1alpha1.Robot) string {
 	var imageBuilder strings.Builder
 	var tagBuilder strings.Builder
 
-	distributions := robot.Spec.Distributions
+	environment := robot.Spec.Environment
 	readyRobot := GetReadyRobotProperties(robot)
 
 	if readyRobot.Enabled {
@@ -49,22 +49,27 @@ func GetImage(node corev1.Node, robot robotv1alpha1.Robot) string {
 	} else {
 
 		registry := "robolaunchio"
-		repository := "robot"
+		repository := ""
 
-		tagBuilder.WriteString("base-")
-		tagBuilder.WriteString(getDistroStr(distributions))
+		if environment == robotv1alpha1.EnvironmentUbuntuFocal {
+			repository = "focal"
+		} else if environment == robotv1alpha1.EnvironmentUbuntuJammy {
+			repository = "jammy"
+		}
 
 		hasGPU := HasGPU(node)
 
 		if hasGPU {
 
-			tagBuilder.WriteString("-agnostic-")
-			tagBuilder.WriteString("xfce") // TODO: make desktop selectable
+			tagBuilder.WriteString("agnostic-")
+			tagBuilder.WriteString("xfce-") // TODO: make desktop selectable
 
 		} else {
-			tagBuilder.WriteString("-agnostic-")
-			tagBuilder.WriteString("xfce") // TODO: make desktop selectable
+			tagBuilder.WriteString("agnostic-")
+			tagBuilder.WriteString("xfce-") // TODO: make desktop selectable
 		}
+
+		tagBuilder.WriteString("amd64")
 
 		imageBuilder.WriteString(filepath.Join(registry, repository) + ":")
 		imageBuilder.WriteString(tagBuilder.String())
@@ -73,24 +78,4 @@ func GetImage(node corev1.Node, robot robotv1alpha1.Robot) string {
 
 	return imageBuilder.String()
 
-}
-
-func getDistroStr(distributions []robotv1alpha1.ROSDistro) string {
-
-	if len(distributions) == 1 {
-		return string(distributions[0])
-	}
-
-	return setPrecisionBetweenDistributions(distributions)
-}
-
-func setPrecisionBetweenDistributions(distributions []robotv1alpha1.ROSDistro) string {
-
-	if distributions[0] == robotv1alpha1.ROSDistroFoxy || distributions[1] == robotv1alpha1.ROSDistroFoxy {
-		return string(robotv1alpha1.ROSDistroFoxy) + "-" + string(robotv1alpha1.ROSDistroGalactic)
-	}
-
-	// TODO: Add validation webhook for distro selections
-	// multi-distro is allowed for only foxy & galactic
-	return ""
 }
